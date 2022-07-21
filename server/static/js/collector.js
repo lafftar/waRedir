93e702621ae904c63770a73ed7a509ef25cb0205
lafftar <lafftar>
2022-07-21 16:22:12 -0400

    quick push

--- server/static/js/collector.js
+++ server/static/js/collector.js
@@ -6,7 +6,7 @@ function makeLink() {
   let number = number_input.value.trim().replace(/\D/g,'');
   let message = message_input.value.trim();
   console.log(countryCode, number, message)
-  let wa_link = `https://api.whatsapp.com/send/?phone=${countryCode}${number}&text=${message}&app_absent=0`
+  let wa_link = `https://web.whatsapp.com/send?phone=${countryCode}${number}&text=${message}&app_absent=1`
   console.log(wa_link)
   document.getElementById('wa_link').innerHTML = wa_link
   document.getElementById('wa_link').href = wa_link
